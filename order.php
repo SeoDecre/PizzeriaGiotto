3ace4004eb0b34c6a5cc11a4df61e65e334f0270
BiriHub <96238643+BiriHub@users.noreply.github.com>
2022-04-08 11:28:06 +0200

    🗃️Initialized 'order.php' file and 'orderHandler.php'

---
+++ order.php
@@ -0,0 +1,58 @@
+
+<?php
+
+    //$_SESSION['connection']
+    ?>
+
+    <html lang="en">
+    <head>
+        <meta charset="UTF-8">
+        <link rel="stylesheet" href="style/main.css">
+        <title>Pizza order </title>
+    </head>
+
+    <body>
+
+    <div class="pizza-menu-container">
+        <?php
+        $connection=$_SESSION['connection'];
+        $counter=0;
+        while ($row=$connection->fetch_row()){
+            // width e height sono temporanei nel tag 'img'
+            // aggiungere i metodi 'removeOne()' e 'addOne()' nel file js
+            
+
+            echo "<div class=\"pizza-menu-element\">
+
+                   <img class=\"pizza-menu-img\" src=\"$row[4]\" alt=\"$row[2]\" width=\"500\" height=\"600\"> 
+                        <div class=\"pizza-menu-info\">
+                            <p>$row[1]</p>
+                            <p>$row[2]</p>
+                            <p>$row[3]€</p>
+                            <div class=\"pizza-menu-buttons\">
+                            <input id=\"$counter.add\" type=\"button\" value=\"Remove one\" onclick=\"removeOne(this.id);\"/>
+                            <input id=\"$counter.rem\" type=\"button\" value=\"Add one\" onclick=\"addOne(this.id);\"/>
+                            </div>
+                          
+                        </div>
+                   </div>
+                 ";
+
+            // da finire
+
+            $counter++;
+                }
+
+
+       //
+       // </div>
+
+        ?>
+    </div>
+    <script>
+        //initialize pizza counter
+        createVar();
+    </script>
+
+    </body>
+    </html>
---
+++ script/orderHandler.js
@@ -0,0 +1,23 @@
+var counterArray;
+
+function createVar() {
+    //per ogni 2 bottoni devo avere un contatore;
+    let nButton=document.getElementsByTagName("button").length/2;
+
+    counterArray=Array(nButton);
+    counterArray.fill(0);
+}
+
+//to do
+
+function removeOne(id){
+
+    // richiamo il contatore per quella specifica pizza
+    //counterArray[]--
+}
+//to do
+function addOne(id){
+
+    // richiamo il contatore per quella specifica pizza
+    //counterArray[]++
+}
